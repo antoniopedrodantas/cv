89032d04158ae89d616ec7cf3bfb57e74133d457
Pedro Dantas <pedro.dantascv@gmail.com>
2020-12-16 14:40:54 +0000

    created scenes folder

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import "./App.css";
 
 // Components
-import Home from "./components/Home";
+import Home from "./scenes/Home";
 
 // Dependencies
 import {  BrowserRouter as Router, Route } from 'react-router-dom';
