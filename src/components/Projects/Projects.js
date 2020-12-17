c6d1b5739740231e6ed0465dd3dab2a2a27baad0
Pedro Dantas <pedro.dantascv@gmail.com>
2020-12-17 15:59:17 +0000

    added some projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+import "./Project.css";
+
 class Projects extends React.Component {
   render() {
     return (
@@ -7,19 +9,58 @@ class Projects extends React.Component {
         <div className="content-left">
           <h1 className="card-title">Projects</h1>
           <div className="card-info">
-            <p>
-              {" "}
-              It is a long established fact that a reader will be distracted by
-              the readable content of a page when looking at its layout. The point
-              of using Lorem Ipsum is that it has a more-or-less normal
-              distribution of letters, as opposed to using 'Content here, content
-              here', making it look like readable English. Many desktop publishing
-              packages and web page editors now use Lorem Ipsum as their default
-              model text, and a search for 'lorem ipsum' will uncover many web
-              sites still in their infancy. Various versions have evolved over the
-              years, sometimes by accident, sometimes on purpose (injected humour
-              and the like).{" "}
-            </p>
+            <div className="wrapper-project">
+              <div className="div1">
+                <h3 className="project-title"> Answerly</h3>
+                <br></br>
+                <p>
+                  {" "}
+                  Answerly was our project for the Web Development course. The
+                  goal was to create an interactive <mark>
+                    Q&A Website
+                  </mark>{" "}
+                  where people could log in, make intersting questions or reply
+                  to them. We used <mark>Laravel</mark> as a framework for this
+                  project.
+                </p>
+                <br></br>
+                <img src="/answerly.PNG" alt="Q&A Project"></img>
+              </div>
+              <div className="div2">
+                <h3 className="project-title"> Lift Management System</h3>
+                <br></br>
+                <p>
+                  {" "}
+                  In Distributed Systems and Artificial Intelligence class we
+                  developed a software that was capable of{" "}
+                  <mark>simulating an skyscraper</mark> environment that
+                  contained lifts. The idea was to be able to introduce certain
+                  independent variables (ex: number of floors, number of lifts,
+                  each lift's max weight) a analyzing data to understand what
+                  would be the most efficient building configuration. This
+                  project was developed in <mark>Java</mark> using Jade and
+                  Repast libraries.
+                </p>
+                <br></br>
+                <img src="/lift-management-system.png" alt="Lift Management System Project"></img>
+              </div>
+              <div className="div3">
+                <h3 className="project-title"> Tempura</h3>
+                <br></br>
+                <p>
+                  Tempura was a fictional company we created for the Information
+                  Systems course. The goal of the project was to develop a web
+                  application for <mark>data analysis</mark> of said company.
+                  The website would have to be able to import SAFT files and
+                  display the enterprise's stats on user-friendly manner. We
+                  used <mark>TypeScript</mark> and <mark>React</mark> fir its
+                  development.
+                </p>
+                <br></br>
+                <img src="/tempura.png" alt="Data Analysis project"></img>
+              </div>
+              <div className="div4"> Div 4</div>
+            </div>
           </div>
         </div>
       </>
