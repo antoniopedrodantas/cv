2ab9e4380cdcb930cb207b40864ecd985fcb6552
Pedro Dantas <pedro.dantascv@gmail.com>
2020-12-17 00:12:55 +0000

    refactored some code; added responsiveness

--- src/scenes/Competences/Competences.js
+++ src/scenes/Competences/Competences.js
@@ -3,14 +3,24 @@ import React from "react";
 import "./Competences.css"
 
 import Navbar from "../../components/Navbar/Navbar";
-import CompetencesComponent from "../../components/CompetencesComponent/CompetencesComponent";
+import Formation from "../../components/Formation/Formation";
+import Accomplishments from "../../components/Accomplishments/Accomplishments";
+import Skills from "../../components/Skills/Skills";
 
 class Competences extends React.Component {
   render() {
     return (
       <>
         <Navbar />
-        <CompetencesComponent />
+        <div className="info">
+            <Formation />
+            <br></br>
+            <hr></hr>
+            <Accomplishments />
+            <br></br>
+            <hr></hr>
+            <Skills />
+        </div>
       </>
     );
   }
