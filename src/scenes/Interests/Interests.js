e97c5dea3d27a3819522d6dff0fe4216247737e9
Pedro Dantas <pedro.dantascv@gmail.com>
2021-02-11 10:30:35 +0000

    tried to fix bug on title property of iframe

--- src/scenes/Interests/Interests.js
+++ src/scenes/Interests/Interests.js
@@ -18,8 +18,8 @@ class Interests extends React.Component {
           </div>
           <div className="spotify-wrapper">
             <div className="spotify-playlist">
-              {/* 
               <iframe
+                title="spotify-playlist"
                 src="https://open.spotify.com/embed/album/3I1J9qKYILlkefjq57izYY"
                 width="300"
                 height="250"
@@ -27,14 +27,13 @@ class Interests extends React.Component {
                 allowtransparency="true"
                 allow="encrypted-media"
               ></iframe>
-              */}
             </div>
             <div className="concert-photo">
               <div className="concert"></div>
             </div>
             <div className="spotify-artist">
-              {/*
               <iframe
+                title="spotify-playlist"
                 src="https://open.spotify.com/follow/1/?uri=spotify:artist:24bD87DnmxOzUG4DrAYsO2?si=3o07y77US-G_PXVTQ4p8Zw&size=detail&theme=light"
                 width="300"
                 height="56"
@@ -43,7 +42,6 @@ class Interests extends React.Component {
                 style={{ border: "none", overflow: "hidden" }}
                 allowtransparency="true"
               ></iframe>
-              */}
             </div>
           </div>
           <div className="friends-wrapper">
