460af1b2065ab5fc1247ae18f1a4cda4b81baac9
Pedro Dantas <pedro.dantascv@gmail.com>
2021-02-11 10:25:58 +0000

    tried deleting spotify widgets

--- src/scenes/Interests/Interests.js
+++ src/scenes/Interests/Interests.js
@@ -18,6 +18,7 @@ class Interests extends React.Component {
           </div>
           <div className="spotify-wrapper">
             <div className="spotify-playlist">
+              {/* 
               <iframe
                 src="https://open.spotify.com/embed/album/3I1J9qKYILlkefjq57izYY"
                 width="300"
@@ -26,11 +27,13 @@ class Interests extends React.Component {
                 allowtransparency="true"
                 allow="encrypted-media"
               ></iframe>
+              */}
             </div>
             <div className="concert-photo">
               <div className="concert"></div>
             </div>
             <div className="spotify-artist">
+              {/*
               <iframe
                 src="https://open.spotify.com/follow/1/?uri=spotify:artist:24bD87DnmxOzUG4DrAYsO2?si=3o07y77US-G_PXVTQ4p8Zw&size=detail&theme=light"
                 width="300"
@@ -40,6 +43,7 @@ class Interests extends React.Component {
                 style={{ border: "none", overflow: "hidden" }}
                 allowtransparency="true"
               ></iframe>
+              */}
             </div>
           </div>
           <div className="friends-wrapper">
