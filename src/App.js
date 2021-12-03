ca490e4998278cbc22f12ef23c6f7409c392cb4e
Pedro Dantas <pedro.dantascv@gmail.com>
2021-12-04 00:43:18 +0100

    removed moving background from mobile version

--- src/App.js
+++ src/App.js
@@ -6,13 +6,40 @@ import LandingPage from "./scenes/LandingPage/LandingPage";
 import SvgBackground from "./components/SvgBackground/SvgBackground";
 
 // Dependencies
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import { LanguageContext } from "./context/LanguageContext";
+
+function getWindowDimensions() {
+  const { innerWidth: width } = window;
+  return width;
+}
 
 function App() {
   // default language
   const [language, setLanguage] = useState("EN");
+
+  const [windowDimensions, setWindowDimensions] = useState(
+    getWindowDimensions()
+  );
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowDimensions(getWindowDimensions());
+    }
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  const renderBackground = () => {
+    if (windowDimensions > 1200) {
+      return <SvgBackground />;
+    }
+    else {
+      return <></>
+    }
+  }
 
   // this file serves as router
   return (
@@ -23,7 +50,7 @@ function App() {
             <Route exact path="/">
               <>
                 <LandingPage />
-                <SvgBackground />
+                {renderBackground()}
               </>
             </Route>
           </Switch>
